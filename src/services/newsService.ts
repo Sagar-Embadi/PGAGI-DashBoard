7223262554cc7a77c5596343eb0800713585b777
Sagar Embadi <sagarembadi7@gmail.com>
2025-05-18 12:07:14 +0530

    trying to bypass cors in news service

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const NEWS_API_KEY = process.env.NEXT_PUBLIC_NEWS_API_KEY;
+const BYPASS_CORS = 'https://cors-anywhere.herokuapp.com/';
 const BASE_URL = 'https://newsapi.org/v2';
 
 export const fetchNews = async (category = 'technology', page = 1) => {
-  const response = await axios.get(`${BASE_URL}/top-headlines`, {
+  const response = await axios.get(`${BYPASS_CORS}${BASE_URL}/top-headlines`, {
     params: {
       category,
       pageSize: 10,
